d4b3acfbda4623df8484bfb0659ca666c26306e2
sergey <serge.dmitriev@gmail.com>
2014-10-20 23:18:32 +0800

    some changes

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -14,11 +14,11 @@ angular.module('project', ['ngRoute', 'firebase'])
     })
     .when('/edit/:projectId', {
       controller:'EditCtrl',
-      templateUrl:'detail.html'
+      templateUrl:'/javascripts/detail.html'
     })
     .when('/new', {
       controller:'CreateCtrl',
-      templateUrl:'detail.html'
+      templateUrl:'/javascripts/detail.html'
     })
     .otherwise({
       redirectTo:'/'
@@ -26,7 +26,47 @@ angular.module('project', ['ngRoute', 'firebase'])
 })
  
 .controller('ListCtrl', function($scope, Projects) {
+
+  
+
+var config = {
+    uri: '1060@' + host,
+    wsServers: 'ws://'+ host +':8088/ws',
+    authorizationUser: '1060',
+    password: 'qwerty',
+    hackIpInContact: true,
+    register: true,
+    log: {
+        builtinEnabled: false,
+    },
+    stunServers: [
+        "stun.ideasip.com",
+        "stun.iptel.org",
+        "stun.rixtelecom.se",
+        "stun.schlund.de",
+        "stunserver.org",
+        "stun.stunprotocol.org:3478",
+        "stun.voiparound.com",
+        "stun.voipbuster.com",
+        "stun.voipstunt.com",
+        "stun.turnservers.com:3478"
+    ],    
+};
+
+
+  $scope.userAgent = new SIP.UA(config);
+
   $scope.projects = Projects;
+
+  $scope.buttonCall = 'Call';
+
+  $scope.call = function(){
+      
+      $scope.userAgent.invite('1060');
+      $scope.buttonCall = 'End';
+  }
+
+
 })
  
 .controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
